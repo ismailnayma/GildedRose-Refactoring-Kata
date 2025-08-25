c1c7a606cdab89867931766929a5de81927acc51
55996 <55996@etu.he2b.be>
2025-08-25 19:11:24 +0200

    refactor if quality minus 50 and replace by increaseQuality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -27,16 +27,12 @@ class GildedRose {
         if (item.name.equals("Aged Brie")) {
             increaseQuality(item);
         }else if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-
-                if (item.sellIn < 11) {
+            increaseQuality(item);
+            if (item.sellIn < 11) {
+                increaseQuality(item);
+            }
+            if (item.sellIn < 6) {
                     increaseQuality(item);
-                }
-
-                if (item.sellIn < 6) {
-                    increaseQuality(item);
-                }
             }
         }else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
             return;
@@ -55,15 +51,13 @@ class GildedRose {
     private static void updateExpired(Item item) {
         if (item.name.equals("Aged Brie")) {
             increaseQuality(item);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            item.quality = 0;
         } else {
-            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
+            if (item.quality > 0) {
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.quality = item.quality - 1;
                 }
-            } else {
-                item.quality = 0;
             }
         }
     }
